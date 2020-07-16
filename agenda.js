649e88131f1063f3d1042fde3817dfb3653d1c99
Jonathan-Meya <jonathanmeya@gmail.com>
2020-07-16 16:42:09 +0300

    first commit

---
+++ agenda.js
@@ -0,0 +1,19 @@
+document.body.style.fontFamily = "Arial";
+
+var agendahead = document.getElementById("agendahead").style.fontFamily = "Arial";
+var agendahead = document.getElementById("agendahead").style.color = "blue";
+
+
+var table = document.getElementById("table");
+table.style.color = "grey";
+table.style.fontFamily="Arial";
+
+
+footer = document.getElementById('footer');
+footer.style.color = 'blue';
+
+
+
+
+
+
